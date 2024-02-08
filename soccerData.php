1791bf4273e2a4ed7f544de2cf334c7a8e0da9e4
Ciara <ccu3@njit.edu>
2024-02-07 19:35:27 -0500

    changed api example

--- soccerData.php
+++ soccerData.php
@@ -1,5 +1,5 @@
 <?php
-$results = shell_exec('GET http://api.football-data.org/alpha/soccerseasons/354');
+$results = shell_exec('GET https://api.football-data.org/v4/teams/86/matches?status=SCHEDULED');
 $arrayCode = json_decode($results);
 var_dump($arrayCode);
 ?>
